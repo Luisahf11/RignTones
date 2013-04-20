655ae0dad583b4b2a12f0273a0559ce37854d2ae
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-04-20 14:22:43 -0500

    0.0.4

--- js/actions.js
+++ js/actions.js
@@ -1,18 +1,26 @@
 // Funcionalidad
 $(document).ready(function(e) {
 	document.addEventListener('deviceready',function(){
-		
 	var src="";
+	var nom="";
     $('#main ul li a').tap(function(){
 		src=$(this).attr('rel');
-		$('#descargar').attr('title',$(this).text());
+		nom=$(this).text();
+		$('#descargar').attr('title',nom);
 	});
 	$('#descargar a').tap (function(){
 		if($(this).text()=='Descargar'){
 			//Descarga de Archivos con transaction
+			var fileTransfer = new FileTransfer();
+			filTransfer.download(src,'file:///mnt/sdcard/ringtoneApp/'+nom+'.mp3',function(entry){
+				//Verificar que no existia el nombre de la carpeta
+				navigator.notification.alert("Archivos Descargado", null, "Completado", "OK");
+			},function(error) {
+				navigator.notification.alert("upload error code" + error.code, null, "Error", "Aceptar");
+			});
 		}
 		if($(this).text()=='Probar'){
-		// Play Media
+			//Play Media
              // Create Media object from src
             my_media = new Media(src, null, function(){
             alert('Error');
