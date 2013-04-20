b63dfbaf953dc3d9fd9a9bd5400444ad8ad5193c
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-04-20 12:18:40 -0500

    0.0.1

---
+++ js/actions.js
@@ -0,0 +1,27 @@
+// Funcionalidad
+$(document).ready(function(e) {
+	document.addEventListener('deviceready',function(){
+		
+	var src="";
+    $('#main ul li a').tap(function(){
+		src=$(this).attr('rel');
+		$('#descargar').attr('title',$(this).text());
+	});
+	$('#descargar a').tap (function(){
+		if($(this).text()=='Descargar'){
+			//Descarga de Archivos con transaction
+		}
+		if($(this).text()=='Probar'){
+		// Play Media
+             // Create Media object from src
+            my_media = new Media(src, null, function(){
+            alert('Error');
+            });
+
+            // Play audio
+            my_media.play();
+    
+		}
+	});
+	},false);
+});
